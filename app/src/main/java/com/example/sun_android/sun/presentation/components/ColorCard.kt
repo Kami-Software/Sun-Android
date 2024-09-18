3182b016efc6a819cbcfbf78ae18391b27b59666
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-18 23:22:34 +0300

    Edit Horizontal Pager

--- app/src/main/java/com/example/sun_android/sun/presentation/components/ColorCard.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/components/ColorCard.kt
@@ -1,6 +1,7 @@
 package com.example.sun_android.sun.presentation.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,8 +14,13 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,21 +35,24 @@ import kotlin.random.Random
 
 fun ColorCard() {
     val pagerState = rememberPagerState(initialPage = 3, pageCount = { 7 })
+    var selectedEmoji by remember { mutableStateOf("😊") } // Başlangıç emojisi
+    var showEmojiPicker by remember { mutableStateOf(false) }
 
     // Generate 7 random colors and remember them
-    val colors = remember {
-        List(7) {
-            Color(
-                red = Random.nextFloat(),
-                green = Random.nextFloat(),
-                blue = Random.nextFloat(),
-                alpha = 1f
-            )
-        }
-    }
+    val colors = listOf(
+        Color.Red,
+        Color.Green,
+        Color.Blue,
+        Color.Yellow,
+        Color.Cyan,
+        Color.Magenta,
+        Color.Gray
+    )
 
     Box(
-        modifier = Modifier.height(300.dp), // Tüm ekranı kaplar
+        modifier = Modifier
+            .height(300.dp)
+            .fillMaxWidth(), // Tüm ekranı kaplar
         contentAlignment = Alignment.Center // İçeriği ortalar
     ) {
         HorizontalPager(
@@ -60,7 +69,7 @@ fun ColorCard() {
                     shape = CircleShape,
                     colors = CardDefaults.cardColors(containerColor = colors[page]),
                     modifier = Modifier
-                        .size(200.dp)
+                        .size(100.dp)
                         .align(Alignment.Center)
                         .graphicsLayer {
                             val pageOffset =
@@ -74,13 +83,18 @@ fun ColorCard() {
                                 scaleY = scale
                             }
                             alpha = lerp(
-                                start = 0.5f,
+                                start = 0.1f,
                                 stop = 1f,
                                 fraction = 1f - pageOffset.coerceIn(0f, 1f)
                             )
-                        }
+                        }.clickable { showEmojiPicker = true } // Tıklanınca emoji seçiciyi göster
                 ) {
-
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = selectedEmoji, style = MaterialTheme.typography.headlineLarge)
+                    }
                 }
             }
         }
