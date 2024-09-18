0491bbe52428efd96f3acdf2203950d7aa653ef4
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-18 21:07:39 +0300

    Edit Horizontal Pager

--- app/src/main/java/com/example/sun_android/sun/presentation/components/ColorCard.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/components/ColorCard.kt
@@ -11,19 +11,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
+import androidx.compose.ui.util.lerp
 import kotlin.random.Random
 
 @Composable
 
-fun ColorCard(){
-    val pagerState = rememberPagerState(pageCount = { 7 } )
+fun ColorCard() {
+    val pagerState = rememberPagerState(initialPage = 3, pageCount = { 7 })
 
     // Generate 7 random colors and remember them
     val colors = remember {
@@ -37,22 +42,47 @@ fun ColorCard(){
         }
     }
 
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.height(300.dp)
+    Box(
+        modifier = Modifier.height(300.dp), // Tüm ekranı kaplar
+        contentAlignment = Alignment.Center // İçeriği ortalar
     ) {
         HorizontalPager(
-            beyondViewportPageCount = colors.size,
             state = pagerState,
             modifier = Modifier.fillMaxWidth()
         ) { page ->
             // Each page content
             Box(
                 modifier = Modifier
-                    .size(200.dp) // Set the size of the circle
-                    .background(color = colors[page], shape = CircleShape)
-            )
+                    .fillMaxWidth(), // Sayfaları genişlik boyunca hizalar
+                contentAlignment = Alignment.Center
+            ) {
+                Card(
+                    shape = CircleShape,
+                    colors = CardDefaults.cardColors(containerColor = colors[page]),
+                    modifier = Modifier
+                        .size(200.dp)
+                        .align(Alignment.Center)
+                        .graphicsLayer {
+                            val pageOffset =
+                                (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
+                            lerp(
+                                start = 0.85f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            ).also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+                            alpha = lerp(
+                                start = 0.5f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            )
+                        }
+                ) {
+
+                }
+            }
         }
     }
 }
