9187a7260411c42c9dd4c6ce8eba01885f8114ba
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-20 23:36:13 +0300

    Edit: ThirdPageContent WeekDays

--- app/src/main/java/com/example/sun_android/sun/presentation/components/ThirdPageContent.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/components/ThirdPageContent.kt
@@ -1,10 +1,14 @@
 package com.example.sun_android.sun.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.CalendarMonth
 import androidx.compose.material.icons.outlined.Palette
@@ -13,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -23,6 +28,8 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun ThirdPageContent() {
+    val weekDays = listOf("M", "T", "W", "T", "F", "S", "S")
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -51,8 +58,40 @@ fun ThirdPageContent() {
             fontWeight = FontWeight.Light,
             color = Color(0xFFB0B0B0), // Lighter gray for subtitle
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(bottom = 10.dp)
+            modifier = Modifier.padding(bottom = 40.dp)
         )
+
+        Text(
+            text = "Repeat",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White, // Lighter gray for subtitle
+            textAlign = TextAlign.Start,
+            modifier = Modifier.padding(bottom = 20.dp, start = 20.dp).align(Alignment.Start)
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            for (i in 1..7) {
+                Box(
+                    modifier = Modifier // Sağ üst köşeden biraz boşluk bırakma
+                        .size(35.dp) // Şeklin boyutu
+                        .clip(CircleShape)
+                        .background(Color.Gray),
+                ) {
+                    Text(
+                        text = weekDays[i - 1],
+                        modifier = Modifier.align(Alignment.Center),
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        fontSize = 16.sp,
+                    )
+                }
+            }
+        }
+
+
     }
 }
 
