7cdc7a312729c89b8f049451e2398a7631e81634
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-21 01:17:26 +0300

    Added: SixthPageContent

--- app/src/main/java/com/example/sun_android/sun/presentation/components/FifthPageContent.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/components/FifthPageContent.kt
@@ -1,16 +1,25 @@
 package com.example.sun_android.sun.presentation.components
 
+import android.icu.text.DateFormat
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.EditCalendar
 import androidx.compose.material.icons.outlined.EventRepeat
 import androidx.compose.material.icons.outlined.Tag
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerFormatter
+import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.DateRangePickerDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,8 +31,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FifthPageContent() {
+    val state = rememberDateRangePickerState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -52,13 +64,41 @@ fun FifthPageContent() {
             fontWeight = FontWeight.Light,
             color = Color(0xFFB0B0B0), // Lighter gray for subtitle
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(bottom = 40.dp)
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+
+        DateRangePicker(state,
+            title = {null},
+            modifier = Modifier,
+            showModeToggle = false,
+            colors = DatePickerDefaults.colors(
+                containerColor = Color.Transparent,
+                headlineContentColor = Color.Transparent,
+                weekdayContentColor = Color.Gray,
+                subheadContentColor = Color.White,
+                yearContentColor = Color.Green,
+                currentYearContentColor = Color.Red,
+                selectedYearContainerColor = Color.Red,
+                disabledDayContentColor = Color.Gray,
+                todayDateBorderColor = Color(0xFFec3557),
+                dayInSelectionRangeContainerColor =  Color.Transparent,//Color(0xFFB0B0B0)
+                dayInSelectionRangeContentColor = Color(0xFFec3557),
+                todayContentColor =Color.White ,
+                disabledSelectedDayContentColor = Color.Gray,
+                navigationContentColor = Color.Yellow,
+                selectedDayContainerColor = Color(0xFFec3557),
+                dayContentColor = Color.White,
+                disabledYearContentColor = Color.Gray,
+                dividerColor = Color.Black,
+                disabledSelectedDayContainerColor = Color.Gray,
+                selectedDayContentColor = Color.White,
+            )
         )
     }
 }
 
 @Composable
 @Preview
-fun FifthPageContentView(){
+fun FifthPageContentView() {
     FifthPageContent()
 }
