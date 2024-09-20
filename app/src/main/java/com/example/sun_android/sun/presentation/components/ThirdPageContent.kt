9d59adb4c8aa1edab500ca68cc3809f1fb73bca6
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-20 23:27:37 +0300

    Edit: ThirdPageContent

--- app/src/main/java/com/example/sun_android/sun/presentation/components/ThirdPageContent.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/components/ThirdPageContent.kt
@@ -1,12 +1,59 @@
 package com.example.sun_android.sun.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.Palette
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun ThirdPageContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.CalendarMonth,
+            contentDescription = "Icon Button",
+            modifier = Modifier.size(100.dp),
+            tint = Color(0xFFec3557)
+        )
 
+        Text(
+            modifier = Modifier.padding(top = 15.dp),
+            text = "Frequency",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Choose which days to repeat this habit",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Light,
+            color = Color(0xFFB0B0B0), // Lighter gray for subtitle
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+    }
 }
 
 @Composable
