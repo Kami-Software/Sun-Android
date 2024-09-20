6c5fb7b92d2aa2d950a2113c857fae0e25e7aae2
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-20 23:44:03 +0300

    Added: FourthPageContent

---
+++ app/src/main/java/com/example/sun_android/sun/presentation/components/FourthPageContent.kt
@@ -0,0 +1,77 @@
+package com.example.sun_android.sun.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun FourthPageContent(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.CalendarMonth,
+            contentDescription = "Icon Button",
+            modifier = Modifier.size(100.dp),
+            tint = Color(0xFFec3557)
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 15.dp),
+            text = "Frequency",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Choose which days to repeat this habit",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Light,
+            color = Color(0xFFB0B0B0), // Lighter gray for subtitle
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 40.dp)
+        )
+
+        Text(
+            text = "Repeat",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White, // Lighter gray for subtitle
+            textAlign = TextAlign.Start,
+            modifier = Modifier.padding(bottom = 20.dp, start = 20.dp).align(Alignment.Start)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun FourthPageContentView(){
+    FourthPageContent()
+}
