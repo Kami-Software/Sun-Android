55520aa066945ad58274510aaef7692b2e7710f4
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-21 18:53:44 +0300

    Added: AppModule

--- app/src/main/java/com/example/sun_android/sun/di/AppModule.kt
+++ app/src/main/java/com/example/sun_android/sun/di/AppModule.kt
@@ -1,4 +1,24 @@
 package com.example.sun_android.sun.di
 
-class AppModule {
+import com.example.sun_android.sun.data.repository.FirebaseHabitRepositoryImpl
+import com.example.sun_android.sun.domain.repository.FirebaseHabitRepository
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun providesFirebaseRepositoryImpl(firestore: FirebaseFirestore): FirebaseHabitRepository {
+        return FirebaseHabitRepositoryImpl(firestore)
+    }
+
+    @Provides
+    @Singleton
+    fun providesFirebaseFirestore() = FirebaseFirestore.getInstance()
 }
