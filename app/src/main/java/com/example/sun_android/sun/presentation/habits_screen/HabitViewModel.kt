b5d4ffe99234384f0fca679e92031971e684fc02
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-21 18:41:06 +0300

    Added: HabitViewModel

--- app/src/main/java/com/example/sun_android/sun/presentation/habits_screen/HabitViewModel.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/habits_screen/HabitViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.sun_android.sun.presentation.habits_screen
 
-class HabitViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sun_android.sun.data.remote.FirebaseHabitEntity
+import com.example.sun_android.sun.domain.repository.FirebaseHabitRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class HabitViewModel @Inject constructor(
+    private val repository: FirebaseHabitRepository
+) : ViewModel() {
+
+    private val _habits = MutableStateFlow<List<FirebaseHabitEntity>>(emptyList())
+    val habits: StateFlow<List<FirebaseHabitEntity>> get() = _habits
+
+    fun loadAllHabits() {
+        viewModelScope.launch {
+            repository.getAllHabits().collect { habitList ->
+                _habits.value = habitList
+            }
+        }
+    }
 }
