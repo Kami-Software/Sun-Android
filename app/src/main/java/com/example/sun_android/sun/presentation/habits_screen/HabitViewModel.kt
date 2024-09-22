5f796ec069625b5d7866ffdab28113e4206a51f1
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-22 14:03:55 +0300

    Edit: HiltViewModel

--- app/src/main/java/com/example/sun_android/sun/presentation/habits_screen/HabitViewModel.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/habits_screen/HabitViewModel.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.sun_android.sun.data.remote.FirebaseHabitEntity
 import com.example.sun_android.sun.domain.repository.FirebaseHabitRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class HabitViewModel @Inject constructor(
     private val repository: FirebaseHabitRepository
 ) : ViewModel() {
@@ -16,6 +18,9 @@ class HabitViewModel @Inject constructor(
     private val _habits = MutableStateFlow<List<FirebaseHabitEntity>>(emptyList())
     val habits: StateFlow<List<FirebaseHabitEntity>> get() = _habits
 
+    init {
+        loadAllHabits()
+    }
     fun loadAllHabits() {
         viewModelScope.launch {
             repository.getAllHabits().collect { habitList ->
