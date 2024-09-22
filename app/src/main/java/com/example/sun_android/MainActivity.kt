0757712d780799b5984ab76e3fd1f10e126529cb
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-09-22 15:26:08 +0300

    Edit: Topbar

--- app/src/main/java/com/example/sun_android/MainActivity.kt
+++ app/src/main/java/com/example/sun_android/MainActivity.kt
@@ -6,20 +6,46 @@ import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -32,9 +58,12 @@ import com.example.sun_android.sun.presentation.swipe_screen.SwipeScreen
 import com.example.sun_android.sun.util.Screens
 import com.example.sun_android.ui.theme.SunAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -53,14 +82,16 @@ class MainActivity : ComponentActivity() {
                 @Suppress("DEPRECATION")
                 window.decorView.systemUiVisibility = 0 // Clear appearance
             }
-
+            // Bugünün tarihini al
+            val currentDate = LocalDate.now()
+            // Günün tam ismini almak için formatla
+            val dayOfWeek = currentDate.format(DateTimeFormatter.ofPattern("EEEE"))
             val currentScreen = remember { mutableStateOf<Screens>(Screens.HabbitsScreen) }
 
             SunAndroidTheme {
                 Surface(
                     modifier = Modifier
                         .fillMaxSize()
-
                 ) {
                     val navController = rememberNavController()
                     LaunchedEffect(navController) {
@@ -76,7 +107,94 @@ class MainActivity : ComponentActivity() {
                     }
                     Scaffold(
                         containerColor = Color.Black,
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(WindowInsets.systemBars.asPaddingValues()),
+                        topBar = {
+                            Row(
+                                modifier = Modifier
+                                    .padding(
+                                        horizontal = 16.dp,
+                                        vertical = 8.dp
+                                    )
+                                    .height(50.dp)
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween // İki öğe arasında boşluk oluştur
+
+                            ) {
+                                Column {
+                                    Row {
+                                        Text(
+                                            text = "Hello, Ovina",
+                                            color = Color.White,
+                                            fontSize = 24.sp,
+                                            modifier = Modifier.padding(end = 3.dp)
+                                        )
+                                        Text(
+                                            text = "\uD83D\uDC4B",
+                                            color = Color.White,
+                                            fontSize = 24.sp,
+                                            modifier = Modifier.padding()
+                                        )
+                                    }
+                                    Row {
+                                        Text(
+                                            text = "$dayOfWeek",
+                                            color = Color.Gray,
+                                            fontSize = 16.sp,
+                                            modifier = Modifier.padding(end = 3.dp)
+                                        )
+                                        Text(
+                                            text = "10 Habits",
+                                            color = Color.White,
+                                            fontSize = 16.sp,
+                                            modifier = Modifier.padding()
+                                        )
+                                    }
+                                }
+                                Row {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxHeight()
+                                            .clip(RoundedCornerShape(30.dp)) // Köşeleri yuvarla
+                                            .background(Color(0xFF2c292a)) // Arka plan rengi
+                                            .border(
+                                                1.dp, Color(0xFF6c6565), RoundedCornerShape(30.dp),
+                                            ),
+                                        contentAlignment = Alignment.Center
+                                    ) {
+
+                                        Text(
+                                            text = "100 \uD83D\uDD25",
+                                            modifier = Modifier
+                                                .align(Alignment.Center)
+                                                .padding(horizontal = 15.dp),
+                                            color = Color.White,
+                                            fontWeight = FontWeight.Bold,
+
+                                            )
+                                    }
+                                    Spacer(modifier = Modifier.width(16.dp))
+                                    IconButton(
+                                        onClick = {},
+                                        modifier = Modifier
+                                            .size(50.dp)
+                                            .clip(shape = CircleShape)
+                                            .background(Color(0xFF2c292a))
+                                            .border(1.dp, Color(0xFF6c6565), CircleShape) // Çerçeve eklendi
+                                    ) {
+                                        Icon(
+                                            imageVector = Icons.Outlined.Settings,
+                                            contentDescription = "Icon Button",
+                                            modifier = Modifier.size(35.dp),
+                                            tint = Color.White
+                                        )
+
+                                    }
+                                }
+
+                            }
+                        },
                         bottomBar = {
                             CustomBottomNavigation(currentScreenId = currentScreen.value.id) { screen ->
                                 currentScreen.value = screen
--- app/src/main/java/com/example/sun_android/sun/presentation/habits_screen/HabbitsScreen.kt
+++ app/src/main/java/com/example/sun_android/sun/presentation/habits_screen/HabbitsScreen.kt
@@ -1,19 +1,38 @@
 package com.example.sun_android.sun.presentation.habits_screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.LocalActivity
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,13 +52,6 @@ fun HabbitsScreen(navController: NavController, viewModel: HabitViewModel = hilt
             .background(Color.Black)
             .padding(16.dp)
     ) {
-        Text(
-            text = "Your Habits",
-            color = Color.White,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
         // LazyVerticalStaggeredGrid to display habits
         LazyVerticalStaggeredGrid(
             columns = StaggeredGridCells.Fixed(2), // Two columns
@@ -57,5 +69,87 @@ fun HabbitsScreen(navController: NavController, viewModel: HabitViewModel = hilt
 @Composable
 @Preview(showBackground = true)
 fun HabbitScreenPreview() {
-    HabbitsScreen(navController = NavController(LocalContext.current))
+    Row(
+        modifier = Modifier
+            .padding(
+                horizontal = 16.dp,
+                vertical = 8.dp
+            )
+            .height(50.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween // İki öğe arasında boşluk oluştur
+
+    ) {
+        Column {
+            Row {
+                Text(
+                    text = "Hello, Ovina",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(end = 3.dp)
+                )
+                Text(
+                    text = "\uD83D\uDC4B",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding()
+                )
+            }
+            Row {
+                Text(
+                    text = "Sunday",
+                    color = Color(0xFFB0B0B0),
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(end = 3.dp)
+                )
+                Text(
+                    text = "10 Habits",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding()
+                )
+            }
+        }
+        Row {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .clip(RoundedCornerShape(30.dp)) // Köşeleri yuvarla
+                    .background(Color(0xFF2c292a)) // Arka plan rengi
+                    .border(
+                        1.dp, Color(0xFF6c6565), RoundedCornerShape(30.dp),
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+
+                Text(
+                    text = "100 \uD83D\uDD25",
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(horizontal = 15.dp),
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+
+                    )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            IconButton(
+                onClick = {},
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(shape = CircleShape)
+                    .background(Color(0xFF2c292a))
+                    .border(1.dp, Color(0xFF6c6565), CircleShape) // Çerçeve eklendi
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Settings,
+                    contentDescription = "Icon Button",
+                    modifier = Modifier.size(35.dp),
+                    tint = Color.White
+                )
+
+            }
+        }
+
+    }
 }
